204bc9572b9d6cb58a91417c73d59fe90246208d
H.J. Keppel <git@rikkeppel.nl>
2025-07-08 20:34:04 +0200

    Moved environment from config.json to own .env file for easier overwrite. Fixed visual bug in mobile columns

--- src/Config.php
+++ src/Config.php
@@ -7,6 +7,10 @@ use \Noodlehaus\Config as NoodleConfig;
 class Config {
     private static NoodleConfig $instance;
 
+    public static function getEnvironment(): string {
+        return getenv('APP_ENV') ?? 'development';
+    }
+
     public static function get(string $key): mixed {
         if (!isset(self::$instance)) {
             self::$instance = NoodleConfig::load(
--- src/Model/EnvironmentEnum.php
+++ src/Model/EnvironmentEnum.php
@@ -7,9 +7,10 @@ use Exception;
 
 enum EnvironmentEnum: string {
     case Development = 'development';
+    case Testing = 'testing';
     case Production = 'production';
 
     public static function getConfigured(): EnvironmentEnum {
-        return self::from(Config::get('environment'));
+        return self::from(Config::getEnvironment());
     }
 }
